c28eb243f82e93ba4c21d34588ba9efab4bbb761
EdgarasB888 <46862483+EdgarasB888@users.noreply.github.com>
2022-09-13 18:58:32 +0300

    Fixed API issue

--- GoldenCinema/Model/TrendingMovie.swift
+++ GoldenCinema/Model/TrendingMovie.swift
@@ -29,7 +29,8 @@ struct TrendingMovieInfo: Codable
     let backdropPath: String?
     let genreIDS: [Int]?
     let id: Int?
-    let originalLanguage: OriginalLanguage?
+    //let originalLanguage: OriginalLanguage?
+    let originalLanguage: String?
     let originalTitle, overview: String?
     let popularity: Double?
     let posterPath, releaseDate, title: String?
